47b31be8c8d48041750c07e9bf67c62307efe959
duoc <git.duoc@duoc.cl>
2023-09-26 09:39:38 -0300

    agregado alerta de cerrar sesion

--- LaPaletiaa/src/app/app.component.ts
+++ LaPaletiaa/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Icon } from 'ionicons/dist/types/components/icon/icon';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,13 +9,38 @@ import { Icon } from 'ionicons/dist/types/components/icon/icon';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Solicitudes', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Mi Auto', url: '/folder/outbox', icon: 'car' },
-    { title: 'Rutas', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Configuracion', url: '/folder/trash', icon: 'trash' },
-    { title: 'Cerrar Sesion', url: '/folder/spam', icon: 'warning' },
+    { title: 'Solicitudes', url: '/folder/Solicitudes', icon: 'albums-outline' },
+    { title: 'Mi Auto', url: '/folder/Mi Auto', icon: 'car-sport-outline' },
+    { title: 'Rutas', url: '/folder/Rutas', icon: 'Rutas' },
+    { title: 'Mensajes', url: '/folder/Mensajes', icon: 'Mensajes' },
+    { title: 'Configuración', url: '/folder/Configuración', icon: 'Configuración' },
+    { title: 'Cerrar Sesión', url: '/folder/Cerrar sesión', icon: 'Cerrar Sesión' },
   ];
   public labels = ['Derechos reservados', 'Politicas de privacidad', 'Terminos y condiciones'];
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+
+  async presentLogoutAlert() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar Sesión',
+      message: '¿Estás seguro de que deseas cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Cerrar Sesión',
+          handler: () => {
+            // Agrega aquí la lógica para cerrar la sesión
+            // Esto podría incluir la eliminación de tokens de autenticación, redirección, etc.
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+  
 }
